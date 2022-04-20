1a6afbaed95bb3050af70d473afad235d0a79454
Balavardhan Reddy Malyala <balavardhanrdy@gmail.com>
2022-04-20 21:14:52 +0530

    code

---
+++ lib/screens/mainPage.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:smart_parking/screens/loginScreen.dart';
+import 'package:smart_parking/utilities/dimensions.dart';
+
+var vpH, vpW, owner = false;
+
+class MainPage extends StatelessWidget {
+  MainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    vpH = getViewportHeight(context);
+    vpW = getViewportWidth(context);
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.blueGrey[300],
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(30.0),
+                child: Text(
+                  "ParkPlace",
+                  style: TextStyle(color: Colors.white, fontSize: 50),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: RaisedButton.icon(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => LoginScreen(isowner: true)),
+                    );
+                  },
+                  color: Colors.blue[900],
+                  icon: Icon(
+                    Icons.directions,
+                    color: Colors.white,
+                  ),
+                  label: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Text(
+                      'Rent your Place',
+                      style:
+                          TextStyle(color: Colors.white, fontSize: vpH * 0.03),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: RaisedButton.icon(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => LoginScreen(isowner: false)),
+                    );
+                  },
+                  color: Colors.pink,
+                  icon: Icon(
+                    Icons.directions,
+                    color: Colors.white,
+                  ),
+                  label: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Text(
+                      'Park your Vehicle',
+                      style:
+                          TextStyle(color: Colors.white, fontSize: vpH * 0.03),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
